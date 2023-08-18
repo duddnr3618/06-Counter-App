a7d53bf5f8dca90656af9fe6308edd83198c8ef5
Youngwook Choi <duddnr3618@gmail.com>
2023-08-18 18:03:02 +0900

    23.08.18 React Counter App 실습

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import Viewer from "./component/Viewer";
+import Controller from "./component/Controller";
+import {useState} from 'react';
+import './App.css';
+
+
+function App() {
+
+  // useState : 상태(값/데이터) -> 변경 -> 랜더링 , 상태값은 부모에서 자식으로만 전달 가능하다.
+  const [count , setCount] = useState(0);     // Viewer 컴포넌트에 전달할 상태값
+
+  // 함수 호출 : 자식 --> 부모로 호출
+  const handleSetCount = (value) => {
+    setCount (count + value);
+  }
+
+
+
+  return (
+    <div className = "App">
+      <h1>Simple Counter</h1>
+      <section>
+      <Viewer count = {count}/>
+      </section>
+
+      <section>
+      <Controller handleSetCount = {handleSetCount}/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
